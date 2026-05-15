0dc765220e1c7747baf1442205d51642df33fc54
Adaku-Henry <henryadaku46@gmail.com>
2026-05-15 00:30:32 -0700

    Standardize project structure for deployment

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -1,7 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-# It MUST have the "config." prefix here
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
