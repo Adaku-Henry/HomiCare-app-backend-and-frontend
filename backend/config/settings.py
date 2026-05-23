804877ac47b64a1f180168e2fc6bba56f1f0d2ba
Adaku-Henry <henryadaku46@gmail.com>
2026-05-23 06:57:54 -0700

    update allowed hosts

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -19,7 +19,11 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    "homicare-app-backend-and-frontend-1.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # =========================================================
